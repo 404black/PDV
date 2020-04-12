9f2f1dcda41b4df59627a2c143f476d603a002a3
Gustavo Jordao <gustavojordaob@outlook.com>
2020-04-12 18:49:01 -0300

    ajustando

---
+++ project404Black/local/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+
+class Local(models.Model):
+    logradouro = models.CharField("endereço", max_length=255, null=True, blank=True)
+    numero = models.CharField("número", max_length=10, blank=True, null=True)
+    complemento = models.CharField("complemento", max_length=100, blank=True, null=True)
+    bairro = models.CharField("bairro",max_length=255, null=True, blank=True)
+    cidade = models.CharField("cidade", max_length=255, null=True, blank=True)
+    estado = models.CharField("estado", max_length=2, null=True, blank=True)
+    cep = models.CharField("cep", max_length=9, null=True, blank=True)
+    geocode = models.CharField("geolocalização (lat,lng)", max_length=32, null=True, blank=True)
+    criado_em = models.DateTimeField("data de criação", editable=False)
+    editado_em = models.DateTimeField("última atualização", null=True, blank=True)
+
+
+    class Meta:
+        ordering = ('logradouro',)
+        verbose_name = 'local'
+        verbose_name_plural = 'locais'
+    def _str_(self):
+        if self.logradouro and self.numero:
+            return self.logradouro + ', ' + self.numero
+        return str(self.id)
+    def save(self, *args, **kwargs):
+        if not self.criado_em:
+            self.criado_em = timezone.now()
+        self.editado_em = timezone.now()
+        return super(Local, self).save(*args, **kwargs)
---
+++ project404Black/pessoa/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+from local.models import Local
+
+class Pessoa(models.Model):
+    nome = models.CharField("nome", max_length=50, null=True, blank=True)
+    cnpj = models.CharField("CNPJ", unique=True, db_index=True, max_length=15, null=True, blank=True)
+    cpf = models.CharField("CPF", unique=True, db_index=True, max_length=15, null=True, blank=True)
+    data_nascimento = models.DateField('data de nascimento', null=True, blank=True)
+    celular = models.CharField("celular", max_length=14, null=True, blank=True)
+    telefone = models.CharField("telefone", max_length=11, null=True, blank=True)
+    local = models.OneToOneField(Local, verbose_name="endereço", on_delete=models.CASCADE,
+    null=True, blank=True)
+
+
+    class Meta:
+        abstract = False
+
+    def _str_(self):
+        if self.nome:
+            return self.nome
+        return str(self.id)
+
+    def get_endereco(self):
+        return self.local._str_()
