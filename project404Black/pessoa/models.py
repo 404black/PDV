86df2f70e53460e5d7a725dda7ddf52e79da322f
user <gmleite20010@gmail.com>
2020-04-14 17:48:32 -0300

    Adcicionei a classe cliente, fornecedor, e funcionario

--- project404Black/pessoa/models.py
+++ project404Black/pessoa/models.py
@@ -13,7 +13,7 @@ class Pessoa(models.Model):
     null=True, blank=True)
 
     class Meta:
-        abstract = False
+        abstract = True
 
     def _str_(self):
         if self.nome:
@@ -22,3 +22,42 @@ class Pessoa(models.Model):
 
     def get_endereco(self):
         return self.local._str_()
+
+class Cliente(Pessoa):
+    data_cadastro = models.DateField('Data de Cadastro')
+    
+    class Meta:
+        ordering = ('nome',)
+        verbose_name = 'cliente'
+        verbose_name_plural = 'clientes'
+    # vamos sobreescrever o método __str__() usando herança
+    def __str__(self):
+        return super().__str__()
+
+class Fornecedor(Pessoa):
+    data_cadastro = models.DateField('Data de Cadastro')
+    status = models.CharField('Status', max_length=10)
+
+    class Meta:
+        ordering = ('nome',)
+        verbose_name = 'fornecedor'
+        verbose_name_plural = 'fornecedores'
+    # vamos sobreescrever o método __str__() usando herança
+    def __str__(self):
+        return super().__str__()
+
+class Funcionario(Pessoa):
+    data_entrada = models.DateField('Data de Entrada', null=True,blank=True)
+    data_saida = models.DateField('Data de Saida', null=True,blank=True)
+    usuario = models.CharField('Usuario', max_length=30, null=True, blank=True)
+    senha = models.CharField('Senha', max_length=30, null=True, blank=True)
+    nivel_acesso = models.CharField('Nivel de Acesso', max_length=50)
+    status = models.CharField('Status', max_length=10)
+    
+    class Meta:
+        ordering = ('nome',)
+        verbose_name = 'funcionario'
+        verbose_name_plural = 'funcionarios'
+    # vamos sobreescrever o método __str__() usando herança
+    def __str__(self):
+        return super().__str__()
