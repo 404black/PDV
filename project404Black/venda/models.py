7a2366e4714256b2374b0fa208aad0a7bee2e627
Gustavo Jordao <gustavojordaob@outlook.com>
2020-04-21 19:13:41 -0300

    tirando on_delete cascade e colocando SET_NULL

--- project404Black/venda/models.py
+++ project404Black/venda/models.py
@@ -9,8 +9,8 @@ class Venda(models.Model):
     data_pagamento = models.DateField('data de pagamento', null=True, blank=True)
     valor_total_venda = models.CharField('valor total venda',  max_length=16, null=True, blank=True)
     forma_pagamento = models.CharField('forma de pagamento',  max_length=10, null=True, blank=True)
-    funcionario = models.ForeignKey(Funcionario, verbose_name="funcionario",null=True, blank=True,on_delete=models.CASCADE)
-    cliente = models.ForeignKey(Cliente, verbose_name="cliente",on_delete=models.CASCADE)
+    funcionario = models.ForeignKey(Funcionario, verbose_name="funcionario",null=True, blank=True,on_delete=models.SET_NULL)
+    cliente = models.ForeignKey(Cliente, verbose_name="cliente", null=True, blank=True,on_delete=models.SET_NULL)
 
     class Meta:
         ordering = ('data_venda',)
@@ -18,8 +18,8 @@ class Venda(models.Model):
         verbose_name_plural = 'vendas'
         
     def __str__(self):
-        if self.data_venda:
-            return 'Data da Venda: ' +  self.data_venda + ', Cliente: ' + self.cliente.nome
+        if self.cliente:
+            return 'Cliente que esta vendendo: ' + self.cliente.nome
         return str(self.id)
 
 
